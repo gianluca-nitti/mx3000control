bf27cbb571e5cc4d1dafe75a2454de008631bf78
Gianluca Nitti <gianluca.nitti96@gmail.com>
2017-11-19 23:31:54 +0100

    Replace hardcoded values with un-encoded version

--- commands/test_keys.c
+++ commands/test_keys.c
@@ -5,6 +5,7 @@
 #include <hidapi/hidapi.h>
 #include "../command.h"
 #include "../util.h"
+#include "../encoding.h"
 
 static int execute(int argc, char** argv, hid_device* dev) {
 
@@ -18,29 +19,33 @@ static int execute(int argc, char** argv, hid_device* dev) {
 	hid_send_feature_report(dev, data4, 9);
 
 	usleep(10000);
-	unsigned char dataA[] = {0, 0, 0x3A, 0x23, 0xB7, 0x37, 0x33, 0x2F, 0xB2};
+	unsigned char dataA[] = {0, 0, 0xF0, 0, 0, 0, 0xF1, 0, 0};
+	encode(dataA);
 	hid_write(dev, dataA, 9);
 	usleep(10000);
-	unsigned char dataB[] = {0, 0, 0x3A, 0x23, 0xB7, 0x47, 0x33, 0x47, 0xB2};
+	unsigned char dataB[] = {0, 0, 0xF2, 0, 0, 0, 0xF4, 0, 0};
+	encode(dataB);
 	hid_write(dev, dataB, 9);
 	usleep(10000);
-	unsigned char dataC[] = {0, 0, 0x3A, 0x23, 0xB7, 0x4F, 0x33, 0xA7, 0xB2};
+	unsigned char dataC[] = {0, 0, 0xF3, 0, 0, 0, 0, 0, 0};
+	encode(dataC);
 	hid_write(dev, dataC, 9);
 	usleep(10000);
-	unsigned char dataD[] = {0, 0, 0xC2, 0x23, 0xB7, 0xB7, 0x33, 0xAF, 0xB5};
+	unsigned char dataD[] = {0, 0, 0, 0, 0, 0x71, 0x1, 0, 0};
+	encode(dataD);
 	hid_write(dev, dataD, 9);
 	usleep(10000);
-	unsigned char dataE[] = {0, 0x71, 0xCA, 0x23, 0xB7, 0xC7, 0x33, 0xAF, 0xB5};
+	unsigned char dataE[] = {0, 0x71, 0x2, 0, 0, 0x72, 0x1, 0, 0};
+	encode(dataE);
 	hid_write(dev, dataE, 9);
 	usleep(10000);
-	unsigned char dataF[] = {0, 0, 0x3A, 0x23, 0xB7, 0xB7, 0x33, 0xA7, 0xB2};
-	hid_write(dev, dataF, 9);
+	unsigned char data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+	encode(data);
+	hid_write(dev, data, 9);
 	usleep(10000);
-	unsigned char dataG[] = {0, 0, 0x3A, 0x23, 0xB7, 0xB7, 0x33, 0xA7, 0xB2};
-	hid_write(dev, dataG, 9);
+	hid_write(dev, data, 9);
 	usleep(10000);
-	unsigned char dataH[] = {0, 0, 0x3A, 0x23, 0xB7, 0xB7, 0x33, 0xA7, 0xB2};
-	hid_write(dev, dataH, 9);
+	hid_write(dev, data, 9);
 
 	usleep(10000);
 	unsigned char data5[] = {0, 0x07, 0x6A, 0x2B, 0xB7, 0xDF, 0x33, 0xA7, 0xB2};
@@ -48,7 +53,6 @@ static int execute(int argc, char** argv, hid_device* dev) {
 
 	for(int i = 0; i < 192; i++) {
 		usleep(10000);
-		unsigned char data[] = {0, 0, 0x3A, 0x23, 0xB7, 0xB7, 0x33, 0xA7, 0xB2};
 		hid_write(dev, data, 9);
 	}
 
