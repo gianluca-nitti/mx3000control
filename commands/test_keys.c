95c8dfc5852f72f3c548798b9d5e00cff6897798
Gianluca Nitti <gianluca.nitti96@gmail.com>
2017-11-26 23:22:54 +0100

    Improve key configuration test command
    The various functions that can be assigned to the mouse buttons are now
    identified by meaningful names.

--- commands/test_keys.c
+++ commands/test_keys.c
@@ -6,39 +6,45 @@
 #include "../command.h"
 #include "../util.h"
 #include "../encoding.h"
+#include "../keymap.h"
+
+static void configure_keys(hid_device* dev, key_config_t config1, key_config_t config2) {
+	unsigned char data[] = {0, config1.byteA, config1.byteB, config1.byteC, config1.byteD,
+		config2.byteA, config2.byteB, config2.byteC, config2.byteD};
+	encode(data);
+	hid_write(dev, data, 9);
+	usleep(10000);
+}
+
+static key_config_t get_key_config(char* keyName) {
+	static int numKeys = sizeof(keys) / sizeof(keys[0]);
+	for (int i = 0; i < numKeys; i++) {
+		if (strcmp(keyName, keys[i].name) == 0) {
+			return keys[i];
+		}
+	}
+	fwprintf(stderr, L"Warning: unknown key \"%s\", setting to \"%s\" instead\n", keyName, keys[0].name);
+	return keys[0];
+}
 
 static int execute(int argc, char** argv, hid_device* dev) {
 
-	unsigned char data1[] = {0, 0x07, 0x3A, 0x2B, 0xB7, 0xCF, 0x33, 0xA7, 0xB2};
+	unsigned char data1[] = {0, 0x07, 0x3A, 0x2B, 0xB7, 0xCF, 0x33, 0xA7, 0xB2}; // 0x07 0x03 ...
 	hid_send_feature_report(dev, data1, 9);
 	usleep(10000);
-	unsigned char data2[] = {0, 0x07, 0x3A, 0x2B, 0xB7, 0xC7, 0x33, 0xA7, 0xB2};
+	unsigned char data2[] = {0, 0x07, 0x3A, 0x2B, 0xB7, 0xC7, 0x33, 0xA7, 0xB2}; // 0x07 0x02 0x01 ...
 	hid_send_feature_report(dev, data2, 9);
 	usleep(10000);
-	unsigned char data4[] = {0, 0x07, 0x3A, 0x2D, 0xB7, 0xD7, 0x33, 0xA7, 0xB2};
-	hid_send_feature_report(dev, data4, 9);
+	unsigned char data3[] = {0, 0x07, 0x3A, 0x2D, 0xB7, 0xD7, 0x33, 0xA7, 0xB2}; // 0x07 0x04 0x01 0x40 ...
+	hid_send_feature_report(dev, data3, 9);
+	usleep(10000);
 
-	usleep(10000);
-	unsigned char dataA[] = {0, 0, 0xF0, 0, 0, 0, 0xF1, 0, 0};
-	encode(dataA);
-	hid_write(dev, dataA, 9);
-	usleep(10000);
-	unsigned char dataB[] = {0, 0, 0xF2, 0, 0, 0, 0xF4, 0, 0};
-	encode(dataB);
-	hid_write(dev, dataB, 9);
-	usleep(10000);
-	unsigned char dataC[] = {0, 0, 0xF3, 0, 0, 0, 0, 0, 0};
-	encode(dataC);
-	hid_write(dev, dataC, 9);
-	usleep(10000);
-	unsigned char dataD[] = {0, 0, 0, 0, 0, 0x71, 0x1, 0, 0};
-	encode(dataD);
-	hid_write(dev, dataD, 9);
-	usleep(10000);
-	unsigned char dataE[] = {0, 0x71, 0x2, 0, 0, 0x72, 0x1, 0, 0};
-	encode(dataE);
-	hid_write(dev, dataE, 9);
-	usleep(10000);
+	configure_keys(dev, get_key_config("click"), get_key_config("menu"));
+	configure_keys(dev, get_key_config("middle-button"), get_key_config("backward"));
+	configure_keys(dev, get_key_config("forward"), get_key_config("disable"));
+	configure_keys(dev, get_key_config("disable"), get_key_config("dpi-up"));
+	configure_keys(dev, get_key_config("dpi-down"), get_key_config("led-color-switch"));
+
 	unsigned char data[] = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 	encode(data);
 	hid_write(dev, data, 9);
---
+++ keymap.h
@@ -0,0 +1,29 @@
+#ifndef _KEYMAP_H
+#define _KEYMAP_H
+
+typedef struct {
+	char* name;
+	unsigned char byteA, byteB, byteC, byteD;
+} key_config_t;
+
+key_config_t keys[] = {
+	{"click", 0, 0xF0, 0, 0},
+	{"middle-button", 0, 0xF2, 0, 0},
+	{"menu", 0, 0xF1, 0, 0},
+	// {"double-click", 0x53, 0, 0x1, 0xA}, //same pattern is used for cut, copy, etc. probably means something like "use macro"
+	{"backward", 0, 0xF4, 0, 0},
+	{"forward", 0, 0xF3, 0, 0},
+	{"dpi-up", 0x71, 0x1, 0, 0},
+	{"dpi-down", 0x71, 0x2, 0, 0},
+	{"led-color-switch", 0x72, 0x1, 0, 0},
+	{"media-play-pause", 0x4, 0, 0, 0xCD},
+	{"media-stop", 0x4, 0, 0, 0xB7},
+	{"media-previous", 0x4, 0, 0, 0xB6},
+	{"media-next", 0x4, 0, 0, 0xB5},
+	{"volume-up", 0x4, 0, 0, 0xE9},
+	{"volume-down", 0x4, 0, 0, 0xEA},
+	{"volume-mute", 0x4, 0, 0, 0xE2},
+	{"disable", 0, 0, 0, 0}
+};
+
+#endif
